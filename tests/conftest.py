9fba9d429fa8afe0ceae22a94721c708bf5454e5
Gary Benson <gary@gbenson.net>
2023-09-27 17:48:39 +0100

    Dynamically choose ports for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,27 @@ from http.client import HTTPConnection
 import pytest
 
 
-@pytest.fixture(params=list(range(6001, 6006)))
+def _open_ipv4_ports():
+    for line in open("/proc/net/tcp").readlines():
+        bits = line.split(":", 3)
+        if len(bits) != 4:
+            continue
+        port = bits[2].split(maxsplit=1)[0]
+        yield int(port, 16)
+
+
+def _ports_to_test(start_port=6000, max_delta=20):
+    ports = list(sorted(_open_ipv4_ports()))
+    for prev_port, port in zip([0] + ports[:-1], ports):
+        delta = port - max(prev_port, start_port)
+        if delta < 1:
+            continue
+        if delta > max_delta:
+            break
+        yield port
+
+
+@pytest.fixture(params=_ports_to_test())
 def port(request):
     yield request.param
 
