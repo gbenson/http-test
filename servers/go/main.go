71ea50796ad099a5614b975fb9c2fe3ea1205b02
Gary Benson <gary@gbenson.net>
2023-09-29 10:26:08 +0100

    Make golang server identifiable

--- servers/go/main.go
+++ servers/go/main.go
@@ -6,11 +6,27 @@ import (
 	"os"
 )
 
+const defaultServerHeader = "gbenson-httptest-golang/1.0"
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("/")))
+	http.Handle("/", ServerHeaderSetter(http.FileServer(http.Dir("/"))))
 	err := http.ListenAndServe("", nil)
 	log.Println(err)
 	if err != http.ErrServerClosed {
 		os.Exit(1)
 	}
+}
+
+func ServerHeaderSetter(handler http.Handler) http.Handler {
+	return HeaderSetter("Server", defaultServerHeader, handler)
+}
+
+func HeaderSetter(key, value string, handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		h := w.Header()
+		if h.Get(key) == "" {
+			h.Set(key, value)
+		}
+		handler.ServeHTTP(w, r)
+	})
 }
